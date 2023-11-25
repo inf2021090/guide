2b5af4e4b6e82f1abcb308e2a76c0d95b68c16a3
inf2021090 <inf2021090@ionio.gr>
2023-11-25 15:30:11 +0000

    updated labs filter for blocks

--- lua/labs.lua
+++ lua/labs.lua
@@ -1,32 +1,38 @@
 function Image(paragraph)
-    local to_string = pandoc.utils.stringify
-    if paragraph.classes:find('labs', 1) then
-        local file = io.open("all_collections/_groups/" .. paragraph.src, 'r')
-        local content = file:read('*a')
-        file:close()
+	local to_string = pandoc.utils.stringify
+	if paragraph.classes:find("labs", 1) then
+		local file = io.open("all_collections/_groups/" .. paragraph.src, "r")
+		local content = file:read("*a")
+		file:close()
 
-        local doc = pandoc.read(content)
+		local doc = pandoc.read(content)
 
-        -- Extract Metadata
-        local meta = doc.meta
-        local link = to_string(meta.link)
-        local title = to_string(meta.title)
+		-- Metadata
+		local meta = doc.meta
+		local link = to_string(meta.link)
+		local title = to_string(meta.title)
 
-        -- Process content after metadata
-        local body = doc.blocks  -- This will contain the content after metadata
-        
-        local md = '#### ' .. title .. '\n_' .. link .. '_\n'
+		-- process content after metadata
+		local body = doc.blocks
 
-        -- Process content blocks (e.g., concatenate paragraphs)
-        for _, block in ipairs(body) do
-            if block.t == "Para" then
-                md = md .. to_string(block)
-            elseif block.t == "Header" then
-                md = md .. '#' .. string.rep('#', block.level) .. ' ' .. to_string(block.content) .. '\n'
-            -- Add more conditions to handle other block types if needed
-            end
-        end
+		local md = "#### " .. title .. "\n\n" .. link .. "\n\n"
 
-        return pandoc.RawInline('markdown', md)
-    end
+		-- process content blocks
+		for _, block in ipairs(body) do
+			if block.t == "Para" then
+				md = md .. to_string(block)
+			elseif block.t == "Header" then
+				md = md .. "#" .. string.rep("#", block.level) .. " " .. to_string(block.content) .. "\n"
+			elseif block.t == "BulletList" then
+				for _, item in ipairs(block.content) do
+					md = md .. "- " .. to_string(item) .. "\n\n"
+				end
+			elseif block.t == "OrderedList" then
+				for _, item in ipairs(block.content) do
+					md = md .. "1. " .. to_string(item) .. "\n"
+				end
+			end
+		end
+		return pandoc.RawInline("markdown", md)
+	end
 end
